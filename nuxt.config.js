b510931f26fbbdd2d163f472b0f816386745be28
Akbar Idria <akbaridria15@gmail.com>
2023-01-14 02:06:28 +0700

    add axios and apexchart plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: "@/plugin/vue-apexchart.js", ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -41,6 +42,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    "@nuxtjs/axios", "@nuxtjs/dotenv"
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
